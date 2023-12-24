00d9731db1539f2d855506337bb9065d12d138a9
preethishree <preethishree@example.com>
2023-12-24 22:35:15 +0530

    first commit

---
+++ src/product.js
@@ -0,0 +1,67 @@
+import img1 from "./assets/img1.jpg";
+import img2 from "./assets/img2.jpg";
+import img3 from "./assets/img3.jpg";
+import img4 from "./assets/img4.jpg";
+import img5 from "./assets/img5.jpg";
+import img6 from "./assets/img6.jpg";
+import img7 from "./assets/img7.jpg";
+import img8 from "./assets/img8.jpg";
+import img9 from "./assets/img9.jpg";
+
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Man's search for Meaning - Vikror E. Frankl",
+    price: 499.0,
+    productImage: img1,
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro 2022 (M1)Surrounded by Idiots - Thomas Erikson",
+    price: 399.0,
+    productImage: img2,
+  },
+  {
+    id: 3,
+    productName: "The Power of your subconscious Mind - Dr Joseph Murphy",
+    price: 299.0,
+    productImage: img4,
+  },
+  {
+    id: 4,
+    productName: "The Art of being Alone - Renuka Gavrani",
+    price: 528.0,
+    productImage: img3,
+  },
+  {
+    id: 5,
+    productName: "Think and Grow Rich - Napoleon Hill",
+    price: 189.99,
+    productImage: img5,
+  },
+  {
+    id: 6,
+    productName: "Wintergirls - Laurie Halse Anderson",
+    price: 168.0,
+    productImage: img6,
+  },
+  {
+    id: 7,
+    productName: "Journey from What If to Why Not - Leena Patil",
+    price: 190.0,
+    productImage: img9,
+  },
+  {
+    id: 8,
+    productName: "The Beautiful Struggle - Ta-Nehisi Coates",
+    price: 400.0,
+    productImage: img7,
+  },
+  {
+    id: 9,
+    productName: "Garnish your Existence - Keshav Toshniwal",
+    price: 450.0,
+    productImage: img8,
+  },
+];
